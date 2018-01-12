53d3a62f44a41248c6616a13c35c5f12dd2881da
adamtre9115 <treadam9115@gmail.com>
2018-01-12 10:18:14 -0500

    update mailgun api add dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var router = express.Router();
-const nodemailer = require("nodemailer")
+const express = require('express');
+const router = express.Router();
+const env = require('dotenv').config();
+const key = process.env.Mail_Key;
+const domain = process.env.Domain;
+
+var mailgun = require("mailgun-js");
+var mailgun = require('mailgun-js')({
+    apiKey: key,
+    domain: domain
+});
 
 router.get('/', function (req, res, next) {
     res.render('pages/index');
@@ -27,41 +35,16 @@ router.post('/message', function (req, res) {
 
         <p>${req.body.message}</p>
     `
-    nodemailer.createTestAccount((err, account) => {
+    var data = {
+        from: 'Infinite Moments Photography <infinitemomentphotos252@gmail>',
+        to: 'infinitemomentphotos252@gmail.com',
+        subject: 'New Response from IMP',
+        text: 'Testing some Mailgun awesomness!',
+        html: output,
+    };
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'infinitemomentphotos252@gmail.com',
-                pass: 'paglskcjlqqmzjywW'
-            },
-        });
-
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: 'infinitemomentphotos252@gmail.com', // sender address
-            to: 'treadam9115@gmail.com', // list of receivers
-            subject: 'New Contact Request from IMP', // Subject line
-            text: 'Hello world?', // plain text body
-            html: output // html body
-        };
-
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            res.render(index, {
-                msg: "Email successfully sent."
-            })
-
-        });
+    mailgun.messages().send(data, function (error, body) {
+        console.log(body);
     });
 
 
